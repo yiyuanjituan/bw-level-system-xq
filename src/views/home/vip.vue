7db5f59a1ebf639000d9d1cc8d22437e9c3d1910
阿苏 <5027849@qq.com>
2026-06-05 16:22:24 +0800

    fix: 开始研究新版本的VIp

--- src/views/home/vip.vue
+++ src/views/home/vip.vue
@@ -23,46 +23,13 @@ function jumpToRecord() {
 
 <template>
   <div class="vip-container">
-    <div class="progress-box">
-      <div class="inner-box">
-        <span class="tips">当前等级</span>
-        <div class="level-image">
-          <img src="/siteadmin/active/color1.avif" alt="." class="icon-img" />
-          <img src="/siteadmin/active/img_dj1.avif" alt="." class="icon-img" />
-          <div class="level-text" style="--vip-level-scale: 1">
-            <span data-text="2">2</span>
-          </div>
-        </div>
-        <div class="progress-content">
-          <div class="content-need">
-            <div class="prefix">距离<i class="vip">VIP 2</i></div>
-            <div class="amount">
-              <span>还需投注</span>
-              <strong class="strong-amount">5,409.14</strong>
-              <div class="loading-icon" @click="handleLoading">
-                <svg-icon name="comm_icon_retry" class-name="text-[#DFBE5B]" :class="{ loading: isLoading }" />
-              </div>
-            </div>
-          </div>
-          <div class="receive-all">
-            <x-button size="small" type="success">一键领取</x-button>
-            <x-button size="small" type="primary" class="ml-[8px]" @click="jumpToRecord">领取记录</x-button>
-          </div>
-        </div>
+    <div class="main-box">
+      <div class="title-box">
+        <svg-icon name="common-event-icon_vip_ms2" class-name="text-[21px] icon-box" />
+        <span class="title-text">VIP奖励列表</span>
+        <svg-icon name="common-event-icon_vip_ms2" class-name="text-[21px] icon-box scale-x-[-1]" />
       </div>
     </div>
-    <div class="sub-title"><p>VIP等级对照表</p></div>
-    <van-tabs shrink animated swipeable>
-      <van-tab title="晋级奖金">
-        <UpLevelGift />
-      </van-tab>
-      <van-tab title="周俸禄">
-        <UpLevelWeek />
-      </van-tab>
-      <van-tab title="VIP特权">
-        <VipLevelPower />
-      </van-tab>
-    </van-tabs>
   </div>
 </template>
 
@@ -71,184 +38,22 @@ function jumpToRecord() {
   background: #000;
   display: flex;
   flex-direction: column;
-
-  .progress-box {
-    height: 90px;
-    padding-top: 10px;
-    padding-left: 10px;
-    padding-right: 10px;
-    .inner-box {
-      background-color: #191919;
-      box-shadow: 0 1.5px 4.5px rgba(0, 0, 0, 0.06);
-      border-radius: 6px;
-      min-height: 80px;
-      padding: 10px;
-      position: relative;
+  .main-box {
+    height: 100%;
+    padding-bottom: 10px;
+    .title-box {
       display: flex;
       align-items: center;
-      justify-content: space-between;
-      box-sizing: border-box;
-
-      .tips {
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: 1;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 16.5px;
-        line-height: 16.5px;
-        padding: 0 5px;
-        color: #fff;
-        font-size: 9px;
-        background-color: #f84673;
-        border-radius: 8px 0;
+      justify-content: center;
+      padding: 10px;
+      .title-text {
+        font-size: 16px;
+        font-weight: 600;
+        color: var(--skin__lead);
+        line-height: 1.4;
       }
-      .level-image {
-        width: 50px;
-        height: 50px;
-        margin: 10px 10px 0 0;
-        position: relative;
-
-        .icon-img {
-          position: absolute;
-          width: 100%;
-          height: 100%;
-          top: 0;
-          left: 0;
-        }
-        .level-text {
-          font-size: 12px;
-          color: transparent;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          zoom: var(--vip-level-scale, 1);
-          width: 50%;
-          text-align: center;
-          font-weight: 700;
-          text-shadow: 0 0.5px 0 rgba(0, 0, 0, 0.4);
-          direction: ltr;
-
-          span {
-            position: relative;
-            display: inline-block;
-            width: 1ch;
-            &::before {
-              content: attr(data-text);
-              background-origin: content-box;
-              clip-path: inset(0.5px);
-              background-repeat: no-repeat;
-              position: absolute;
-              top: 0;
-              left: 0;
-              z-index: 0;
-              width: 100%;
-              height: 100%;
-              text-shadow: none;
-              background-image: linear-gradient(to bottom, #f7ea94, #e5b952 51%, #ce9510);
-              background-size: cover;
-              -webkit-background-clip: text;
-            }
-          }
-        }
-      }
-    }
-  }
-
-  .progress-content {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    flex: 1;
-    .content-need {
-      margin-bottom: 0;
-      padding-top: 10px;
-      font-size: 12px;
-      color: #656565;
-      text-align: left;
-      .prefix {
-        margin-bottom: 5px;
-        .vip {
-          margin: 0 2.5px;
-          color: #f84673;
-          font-weight: 700;
-        }
-      }
-      .amount {
-        font-size: 12px;
-        color: #656565;
-        display: flex;
-        align-items: center;
-
-        .strong-amount {
-          margin-left: 5px;
-          color: white;
-        }
-        .loading-icon {
-          margin-left: 5px;
-          .loading {
-            animation: loading-icon 0.5s ease-in-out infinite;
-          }
-          @keyframes loading-icon {
-            from {
-              transform: rotate(0deg);
-            }
-            to {
-              transform: rotate(360deg);
-            }
-          }
-        }
-      }
-    }
-    .receive-all {
-      .receive-btn {
-        width: 60px;
-        height: 25px;
-        margin-right: 0;
-        border-radius: 4px;
-        border-color: transparent;
-
-        :deep(.ui-button__text) {
-          white-space: nowrap;
-          font-size: 10px;
-          color: white;
-        }
-      }
-    }
-  }
-
-  .sub-title {
-    margin: 25px 10px 0;
-    color: #ffffff;
-    font-size: 15px;
-    text-align: center;
-    line-height: 1.6;
-  }
-  :deep(.van-tabs) {
-    --van-padding-xs: 0px;
-    --van-tabs-nav-background: transparent;
-    --van-tab-text-color: white;
-    --van-tab-active-text-color: var(--van-tabs-bottom-bar-color);
-    --van-font-bold: normal;
-    --van-tabs-line-height: 45px;
-    --van-tabs-bottom-bar-height: 2px;
-    //--van-tabs-bottom-bar-width: 48px;
-
-    height: 100%;
-
-    .van-tab {
-      --van-padding-xs: 15px;
-      font-size: 12px;
-      padding-top: 10px;
-    }
-
-    .van-tabs__content {
-      height: calc(100% - 45px);
-      .van-tab__panel {
-        height: 100%;
+      .icon-box {
+        color: var(--skin__neutral_2);
       }
     }
   }
